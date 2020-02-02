98cbd79f1c3a8cec8b363016849272d79cddf776
MF <m.fehrenbach@pm.me>
2020-02-02 19:18:08 +0000

    Added debug logs to ArchiveAutoService::CreateBSA

--- src/ArchiveAutoService.cpp
+++ src/ArchiveAutoService.cpp
@@ -1,9 +1,13 @@
 #include <bsapacker/ArchiveAutoService.h>
 
+#include <QDebug>
+
 namespace BsaPacker
 {
 	void ArchiveAutoService::CreateBSA(libbsarch::bs_archive_auto* archive, const QString& archiveName) const
 	{
+		qDebug() << "Saving archive " + archiveName + " to disk";
 		archive->save_to_disk(archiveName.toStdString());
+		qDebug() << "Finished writing archive " + archiveName + " to disk";
 	}
 } // namespace BsaPacker
