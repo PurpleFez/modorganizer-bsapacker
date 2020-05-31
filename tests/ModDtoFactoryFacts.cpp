26d158462688ee4970043a6f1fc0b35dcc64bc5f
MF <m.fehrenbach@pm.me>
2020-05-31 13:54:48 +0100

    Added failing test

--- tests/ModDtoFactoryFacts.cpp
+++ tests/ModDtoFactoryFacts.cpp
@@ -1,10 +1,13 @@
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
 
-#include "ModDtoFactory.h"
+#include <bsapacker/ModDtoFactory.h>
+#include "MockModContext.h"
+#include "MockPackerDialog.h"
 
 using namespace BsaPacker;
 using ::testing::NaggyMock;
+using ::testing::Return;
 
 namespace BsaPackerTests
 {
@@ -12,13 +15,32 @@ namespace BsaPackerTests
 	{
 	protected:
 		void SetUp() override {
+			naggyMockModContext = new NaggyMock<MockModContext>();
+			naggyMockPackerDialog = new NaggyMock<MockPackerDialog>();
 		}
 
 		void TearDown() override {
+			delete naggyMockModContext;
+			delete naggyMockPackerDialog;
 		}
+
+		NaggyMock<MockModContext>* naggyMockModContext;
+		NaggyMock<MockPackerDialog>* naggyMockPackerDialog;
 	};
 
-	TEST_F(ModDtoFactoryFacts, Ctor_Always_Constructs)
+	TEST_F(ModDtoFactoryFacts, Create_WhenGameIsNewVegas_ReturnsNewVegasIdInDto)
 	{
+		// Arrange
+		auto sut = ModDtoFactory(
+			naggyMockModContext,
+			naggyMockPackerDialog
+		);
+
+		ON_CALL(*naggyMockModContext, GetNexusId())
+			.WillByDefault(Return(1));
+
+		auto result = sut.Create();
+
+		EXPECT_EQ(1, result->NexusId());
 	}
 }
