bee84f30995133a60c23c42477883bb4e802c57a
MF <m.fehrenbach@pm.me>
2020-06-03 19:32:45 +0100

    Added tests for GetArchiveTypes

--- tests/ArchiveBuilderFactoryFacts.cpp
+++ tests/ArchiveBuilderFactoryFacts.cpp
@@ -4,6 +4,7 @@
 #include <bsapacker/ArchiveBuilderFactory.h>
 #include "MockArchiveBuilderHelper.h"
 #include "ModDto.h"
+#include "NexusId.h"
 #include <bsapacker/NullArchiveBuilder.h>
 #include <bsapacker/GeneralArchiveBuilder.h>
 #include <bsapacker/TexturelessArchiveBuilder.h>
@@ -36,6 +37,98 @@ namespace BsaPackerTests
 		);
 	}
 
+	TEST_F(ArchiveBuilderFactoryFacts, GetArchiveTypes_WhenNexusIdIsMorrowind_ReturnsbaTES3)
+	{
+		// Arrange
+		auto sut = ArchiveBuilderFactory(naggyMockArchiveBuilderHelper);
+		auto dto = ModDto(NexusId::Morrowind, "", "", "");
+
+		// Act
+		auto result = sut.GetArchiveTypes(&dto);
+
+		// Assert
+		EXPECT_EQ(bsa_archive_type_e::baTES3, result.at(0));
+	}
+
+	TEST_F(ArchiveBuilderFactoryFacts, GetArchiveTypes_WhenNexusIdIsOblivion_ReturnsbaTES4)
+	{
+		// Arrange
+		auto sut = ArchiveBuilderFactory(naggyMockArchiveBuilderHelper);
+		auto dto = ModDto(NexusId::Oblivion, "", "", "");
+
+		// Act
+		auto result = sut.GetArchiveTypes(&dto);
+
+		// Assert
+		EXPECT_EQ(bsa_archive_type_e::baTES4, result.at(0));
+	}
+
+	TEST_F(ArchiveBuilderFactoryFacts, GetArchiveTypes_WhenNexusIdIsFallout3_ReturnsbaFO3)
+	{
+		// Arrange
+		auto sut = ArchiveBuilderFactory(naggyMockArchiveBuilderHelper);
+		auto dto = ModDto(NexusId::Fallout3, "", "", "");
+
+		// Act
+		auto result = sut.GetArchiveTypes(&dto);
+
+		// Assert
+		EXPECT_EQ(bsa_archive_type_e::baFO3, result.at(0));
+	}
+
+	TEST_F(ArchiveBuilderFactoryFacts, GetArchiveTypes_WhenNexusIdIsNewVegas_ReturnsbaFO3)
+	{
+		// Arrange
+		auto sut = ArchiveBuilderFactory(naggyMockArchiveBuilderHelper);
+		auto dto = ModDto(NexusId::NewVegas, "", "", "");
+
+		// Act
+		auto result = sut.GetArchiveTypes(&dto);
+
+		// Assert
+		EXPECT_EQ(bsa_archive_type_e::baFO3, result.at(0));
+	}
+
+	TEST_F(ArchiveBuilderFactoryFacts, GetArchiveTypes_WhenNexusIdIsSkyrim_ReturnsbaFO3)
+	{
+		// Arrange
+		auto sut = ArchiveBuilderFactory(naggyMockArchiveBuilderHelper);
+		auto dto = ModDto(NexusId::Skyrim, "", "", "");
+
+		// Act
+		auto result = sut.GetArchiveTypes(&dto);
+
+		// Assert
+		EXPECT_EQ(bsa_archive_type_e::baFO3, result.at(0));
+	}
+
+	TEST_F(ArchiveBuilderFactoryFacts, GetArchiveTypes_WhenNexusIdIsSkyrimSE_ReturnsbaSSE)
+	{
+		// Arrange
+		auto sut = ArchiveBuilderFactory(naggyMockArchiveBuilderHelper);
+		auto dto = ModDto(NexusId::SkyrimSE, "", "", "");
+
+		// Act
+		auto result = sut.GetArchiveTypes(&dto);
+
+		// Assert
+		EXPECT_EQ(bsa_archive_type_e::baSSE, result.at(0));
+	}
+
+	TEST_F(ArchiveBuilderFactoryFacts, GetArchiveTypes_WhenNexusIdIsFallout4_ReturnsbaFO4andbaFO4dds)
+	{
+		// Arrange
+		auto sut = ArchiveBuilderFactory(naggyMockArchiveBuilderHelper);
+		auto dto = ModDto(NexusId::Fallout4, "", "", "");
+
+		// Act
+		auto result = sut.GetArchiveTypes(&dto);
+
+		// Assert
+		EXPECT_EQ(bsa_archive_type_e::baFO4, result.at(0));
+		EXPECT_EQ(bsa_archive_type_e::baFO4dds, result.at(1));
+	}
+
 	TEST_F(ArchiveBuilderFactoryFacts, Create_WhenArchiveTypeNone_ReturnsNullArchiveBuilder)
 	{
 		// Arrange
@@ -66,7 +159,7 @@ namespace BsaPackerTests
 		EXPECT_STREQ(typeid(GeneralArchiveBuilder).name(), typeid(*resultType).name());
 	}
 
-		TEST_F(ArchiveBuilderFactoryFacts, Create_WhenArchiveTypeTES4_ReturnsGeneralArchiveBuilder)
+	TEST_F(ArchiveBuilderFactoryFacts, Create_WhenArchiveTypeTES4_ReturnsGeneralArchiveBuilder)
 	{
 		// Arrange
 		auto sut = ArchiveBuilderFactory(naggyMockArchiveBuilderHelper);
@@ -79,5 +172,65 @@ namespace BsaPackerTests
 		// Assert
 		auto resultType = dynamic_cast<GeneralArchiveBuilder*>(result);
 		EXPECT_STREQ(typeid(GeneralArchiveBuilder).name(), typeid(*resultType).name());
+	}
+
+	TEST_F(ArchiveBuilderFactoryFacts, Create_WhenArchiveTypeFO3_ReturnsGeneralArchiveBuilder)
+	{
+		// Arrange
+		auto sut = ArchiveBuilderFactory(naggyMockArchiveBuilderHelper);
+		auto type = bsa_archive_type_e::baFO3;
+
+		// Act
+		auto returned = sut.Create(type, &ModDto(0, "", "", ""));
+		auto result = returned.get();
+
+		// Assert
+		auto resultType = dynamic_cast<GeneralArchiveBuilder*>(result);
+		EXPECT_STREQ(typeid(GeneralArchiveBuilder).name(), typeid(*resultType).name());
+	}
+
+	TEST_F(ArchiveBuilderFactoryFacts, Create_WhenArchiveTypeSSE_ReturnsGeneralArchiveBuilder)
+	{
+		// Arrange
+		auto sut = ArchiveBuilderFactory(naggyMockArchiveBuilderHelper);
+		auto type = bsa_archive_type_e::baSSE;
+
+		// Act
+		auto returned = sut.Create(type, &ModDto(0, "", "", ""));
+		auto result = returned.get();
+
+		// Assert
+		auto resultType = dynamic_cast<GeneralArchiveBuilder*>(result);
+		EXPECT_STREQ(typeid(GeneralArchiveBuilder).name(), typeid(*resultType).name());
+	}
+
+	TEST_F(ArchiveBuilderFactoryFacts, Create_WhenArchiveTypeFO4_ReturnsTexturelessArchiveBuilder)
+	{
+		// Arrange
+		auto sut = ArchiveBuilderFactory(naggyMockArchiveBuilderHelper);
+		auto type = bsa_archive_type_e::baFO4;
+
+		// Act
+		auto returned = sut.Create(type, &ModDto(0, "", "", ""));
+		auto result = returned.get();
+
+		// Assert
+		auto resultType = dynamic_cast<TexturelessArchiveBuilder*>(result);
+		EXPECT_STREQ(typeid(TexturelessArchiveBuilder).name(), typeid(*resultType).name());
+	}
+
+	TEST_F(ArchiveBuilderFactoryFacts, Create_WhenArchiveTypeFO4dds_ReturnsTextureArchiveBuilder)
+	{
+		// Arrange
+		auto sut = ArchiveBuilderFactory(naggyMockArchiveBuilderHelper);
+		auto type = bsa_archive_type_e::baFO4dds;
+
+		// Act
+		auto returned = sut.Create(type, &ModDto(0, "", "", ""));
+		auto result = returned.get();
+
+		// Assert
+		auto resultType = dynamic_cast<TextureArchiveBuilder*>(result);
+		EXPECT_STREQ(typeid(TextureArchiveBuilder).name(), typeid(*resultType).name());
 	}
 }
