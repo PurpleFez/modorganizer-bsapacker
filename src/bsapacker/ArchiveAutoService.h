b40a9b91d44616ee703faaa8a5b64c19dba603dc
MF <m.fehrenbach@pm.me>
2020-06-08 20:33:47 +0100

    Added ArchiveAutoService test

--- src/bsapacker/ArchiveAutoService.h
+++ src/bsapacker/ArchiveAutoService.h
@@ -1,22 +1,15 @@
 #ifndef ARCHIVEAUTOSERVICE_H
 #define ARCHIVEAUTOSERVICE_H
 
-#include <bsapacker/IModDto.h>
+#include "bsapacker_global.h"
 #include <bsapacker/IArchiveAutoService.h>
-#include <qlibbsarch/BSArchiveAuto.h>
 
 namespace BsaPacker
 {
-	class ArchiveAutoService : public IArchiveAutoService
+	class BSAPACKER_EXPORT ArchiveAutoService : public IArchiveAutoService
 	{
 	public:
 		ArchiveAutoService() = default;
-		~ArchiveAutoService() override = default;
-		ArchiveAutoService(const ArchiveAutoService&) = delete;
-		ArchiveAutoService& operator=(const ArchiveAutoService&) = delete;
-		ArchiveAutoService(ArchiveAutoService&&) = default;
-		ArchiveAutoService& operator=(ArchiveAutoService&&) = default;
-
 		void CreateBSA(BSArchiveAuto* archive, const QString& archiveName, bsa_archive_type_e type) const override;
 	};
 } // namespace BsaPacker
